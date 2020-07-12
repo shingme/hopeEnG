4de376cef5c5ba36022ef1f861cc5c8ea529647c
shingme <sinmi1620@gmail.com>
2020-07-12 20:45:29 +0900

    문의 게시판 검색 기능 추가

--- src/main/java/org/hope/web/service/BraaService.java
+++ src/main/java/org/hope/web/service/BraaService.java
@@ -1,12 +1,13 @@
 package org.hope.web.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.hope.web.domain.BraaVO;
 
 public interface BraaService {
 	public void insertBraa(BraaVO braaVO);
 
-	public List<BraaVO> selectBraa(BraaVO braaVO);
+	public List<BraaVO> selectBraa(Map<String, String> map);
 
 }
