dc62f6c3babe45e70fe6a8be2f4b8b02f2b07e29
hjkim <wlrhkvlf23@gmail.com>
2020-07-21 02:22:19 +0900

    GlaaVO 추가

---
+++ src/main/java/org/hope/web/domain/GlaaVO.java
@@ -0,0 +1,57 @@
+package org.hope.web.domain;
+
+public class GlaaVO {
+	private int glNo;
+	private String glNm;
+	private String glCts;
+	private String filePath;
+	private String showMainYn;
+	private String regiDate;
+	private String modyDate;
+	
+	
+	public int getGlNo() {
+		return glNo;
+	}
+	public void setGlNo(int glNo) {
+		this.glNo = glNo;
+	}
+	public String getGlNm() {
+		return glNm;
+	}
+	public void setGlNm(String glNm) {
+		this.glNm = glNm;
+	}
+	public String getGlCts() {
+		return glCts;
+	}
+	public void setGlCts(String glCts) {
+		this.glCts = glCts;
+	}
+	public String getFilePath() {
+		return filePath;
+	}
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+	public String getShowMainYn() {
+		return showMainYn;
+	}
+	public void setShowMainYn(String showMainYn) {
+		this.showMainYn = showMainYn;
+	}
+	public String getRegiDate() {
+		return regiDate;
+	}
+	public void setRegiDate(String regiDate) {
+		this.regiDate = regiDate;
+	}
+	public String getModyDate() {
+		return modyDate;
+	}
+	public void setModyDate(String modyDate) {
+		this.modyDate = modyDate;
+	}
+	
+	
+}
